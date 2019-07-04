e2cc88755e6f3c375ce9ea370761dc860843ba43
Przemyslaw Lal <przemyslawx.lal@intel.com>
2019-07-04 08:32:42 +0100

    set proxy build-args only if the variables are present

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -18,12 +18,14 @@
 
 set -e
 
+# set proxy args
+BUILD_ARGS=()
+[ ! -z "$http_proxy"  ] && BUILD_ARGS+=("--build-arg http_proxy=$http_proxy")
+[ ! -z "$HTTP_PROXY"  ] && BUILD_ARGS+=("--build-arg HTTP_PROXY=$HTTP_PROXY")
+[ ! -z "$https_proxy" ] && BUILD_ARGS+=("--build-arg https_proxy=$https_proxy")
+[ ! -z "$HTTPS_PROXY" ] && BUILD_ARGS+=("--build-arg HTTPS_PROXY=$HTTPS_PROXY")
+[ ! -z "$no_proxy"    ] && BUILD_ARGS+=("--build-arg no_proxy=$no_proxy")
+[ ! -z "$NO_PROXY"    ] && BUILD_ARGS+=("--build-arg NO_PROXY=$NO_PROXY")
+
 # build admission controller Docker image
-docker build --build-arg http_proxy=${http_proxy} \
-             --build-arg HTTP_PROXY=${HTTP_PROXY} \
-             --build-arg https_proxy=${https_proxy} \
-             --build-arg HTTPS_PROXY=${HTTPS_PROXY} \
-             --build-arg no_proxy=${no_proxy} \
-             --build-arg NO_PROXY=${NO_PROXY} \
-             -f Dockerfile \
-             -t network-resources-injector .
+docker build ${BUILD_ARGS[@]} -f Dockerfile -t network-resources-injector .
