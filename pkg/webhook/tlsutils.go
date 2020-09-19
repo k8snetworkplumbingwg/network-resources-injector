5b32e32dc6e193e044c587c4fb5a63f3a2d01da9
Kennelly, Martin <martin.kennelly@intel.com>
2020-09-19 21:35:41 +0100

    Harden server TLS endpoint
    Allow user to add one or more client CAs to NRI server TLS
    endpoint.
    
    Signed-off-by: Kennelly, Martin <martin.kennelly@intel.com>

--- pkg/webhook/tlsutils.go
+++ pkg/webhook/tlsutils.go
@@ -16,6 +16,9 @@ package webhook
 
 import (
 	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
 	"sync"
 
 	"github.com/golang/glog"
@@ -26,6 +29,23 @@ type tlsKeypairReloader struct {
 	cert      *tls.Certificate
 	certPath  string
 	keyPath   string
+}
+
+type clientCertPool struct {
+	certPool  *x509.CertPool
+	certPaths *ClientCAFlags
+	insecure  bool
+}
+
+type ClientCAFlags []string
+
+func (i *ClientCAFlags) String() string {
+	return ""
+}
+
+func (i *ClientCAFlags) Set(path string) error {
+	*i = append(*i, path)
+	return nil
 }
 
 func (keyPair *tlsKeypairReloader) Reload() error {
@@ -61,4 +81,60 @@ func NewTlsKeypairReloader(certPath, keyPath string) (*tlsKeypairReloader, error
 	result.cert = &cert
 
 	return result, nil
+}
+
+//NewClientCertPool will load a single client CA
+func NewClientCertPool(clientCaPaths *ClientCAFlags, insecure bool) (*clientCertPool, error) {
+	pool := &clientCertPool{
+		certPaths: clientCaPaths,
+		insecure: insecure,
+	}
+	if !pool.insecure {
+		if err := pool.Load(); err != nil {
+			return nil, err
+		}
+	}
+	return pool, nil
+}
+
+//Load a certificate into the client CA pool
+func (pool *clientCertPool) Load() error {
+	if pool.insecure {
+		glog.Infof("can not load client CA pool. Remove --insecure flag to enable.")
+		return nil
+	}
+
+	if len(*pool.certPaths) == 0 {
+		return fmt.Errorf("no client CA file path(s) found")
+	}
+
+	pool.certPool = x509.NewCertPool()
+	for _, path := range *pool.certPaths {
+		caCertPem, err := ioutil.ReadFile(path)
+		if err != nil {
+			return fmt.Errorf("failed to load client CA file from path '%s'", path)
+		}
+		if ok := pool.certPool.AppendCertsFromPEM(caCertPem); !ok {
+			return fmt.Errorf("failed to parse client CA file from path '%s'", path)
+		}
+		glog.Infof("added client CA to cert pool from path '%s'", path)
+	}
+	glog.Infof("added '%d' client CA(s) to cert pool", len(*pool.certPaths))
+	return nil
+}
+
+//GetCertPool returns a client CA pool
+func (pool *clientCertPool) GetCertPool() *x509.CertPool {
+	if pool.insecure {
+		return nil
+	}
+	return pool.certPool
+}
+
+//GetClientAuth determines the policy the http server will follow for TLS Client Authentication
+func GetClientAuth(insecure bool) tls.ClientAuthType {
+	if insecure {
+		return tls.NoClientCert
+	}
+	return tls.RequireAndVerifyClientCert
 }
