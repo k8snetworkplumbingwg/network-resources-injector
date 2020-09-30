dc089fec38c0be69674ed25fa9b437ecb8f7e492
Periyasamy Palanisamy <periyasamy.palanisamy@est.tech>
2020-09-30 10:40:47 +0200

    retrieve ca client bundle from extension api server config map
    This change updates the script with appropriate command so that
    ca cert bundle is retrieved properly so that mutating admission
    webhook can be configured correctly.
    
    Signed-off-by: Periyasamy Palanisamy <periyasamy.palanisamy@est.tech>

--- scripts/webhook-patch-ca-bundle.sh
+++ scripts/webhook-patch-ca-bundle.sh
@@ -7,7 +7,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-export CA_BUNDLE=$(kubectl get configmap -n kube-system extension-apiserver-authentication -o=jsonpath='{.data.client-ca-file}' | base64 -w=0)
+export CA_BUNDLE=$(kubectl get configmap -n kube-system extension-apiserver-authentication -o=jsonpath='{.data.client-ca-file}' | base64 --w=0)
 
 if command -v envsubst >/dev/null 2>&1; then
     envsubst
