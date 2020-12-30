0eff8c88155b729b403f64c34c498258d0f96cde
Periyasamy Palanisamy <periyasamy.palanisamy@est.tech>
2020-12-30 11:46:06 +0100

    fix review comments
    Signed-off-by: Periyasamy Palanisamy <periyasamy.palanisamy@est.tech>

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -39,7 +39,7 @@ func main() {
 	injectHugepageDownApi := flag.Bool("injectHugepageDownApi", false, "Enable hugepage requests and limits into Downward API.")
 	flag.Var(&clientCAPaths, "client-ca", "File containing client CA. This flag is repeatable if more than one client CA needs to be added to server")
 	resourceNameKeys := flag.String("network-resource-name-keys", "k8s.v1.cni.cncf.io/resourceName", "comma separated resource name keys --network-resource-name-keys.")
-	resourceHonorFlag := flag.Bool("honor-resource", false, "Honor the existing resources --honor-resource")
+	resourcesHonorFlag := flag.Bool("honor-resources", false, "Honor the existing requested resources requests & limits --honor-resources")
 	flag.Parse()
 
 	if *port < 1024 || *port > 65535 {
@@ -71,7 +71,7 @@ func main() {
 
 	webhook.SetInjectHugepageDownApi(*injectHugepageDownApi)
 
-	webhook.SetHonorExistingResources(*resourceHonorFlag)
+	webhook.SetHonorExistingResources(*resourcesHonorFlag)
 
 	err = webhook.SetResourceNameKeys(*resourceNameKeys)
 	if err != nil {
